6b7d9f177ab7e2fb320efdc391386c47534d8da1
Islam Mankiev <mankiev94@yandex.ru>
2026-09-01 13:25:02 +0300

    fixed

--- homework-10.js
+++ homework-10.js
@@ -1,21 +1,38 @@
-productCard = [
-  {
-    cardImage: url("./img/moisturizing-mousse.png"),
-    cardCategory: 'для нормальной кожи',
-    cardName: 'Увлажняющий мусс',
-    cardDescription: 'Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.',
-    cardStructure: 'Состав:',
-    structureList: 'активные натуральные комплексы' +
-    'витамины С, А, РР, В И Е' +
-    'солнцезащитные компоненты',
-    cardPrice: 'Цена' + 
-    2750 + '₽'
-  }
-]
+import { productCard } from './product-cards.js'
 
 const productList = document.getElementById('product-list')
-const products = document.getElementById('products')
+const products = document.querySelector('.products')
 
 productCard.forEach(product => {
   const productCopy = productList.content.cloneNode(true)
+  productCopy.querySelector('.card__image').src = product.cardImage
+  productCopy.querySelector('.card__category').textContent = product.cardCategory
+  productCopy.querySelector('.card__name').textContent = product.cardName
+  productCopy.querySelector('.card__description').textContent = product.cardDescription
+  productCopy.querySelector('.card__structure').textContent = product.cardStructure
+  productCopy.querySelector('.structure__list').textContent = product.structureList
+  productCopy.querySelector('.card__price').textContent = product.cardPrice
+  products.appendChild(productCopy)
 })
+  
+const keyAndName = productCard.reduce ((acc, card) => {
+  acc.push({
+  [productCard.cardName] : productCard.cardDescription
+  })
+  
+  return acc
+}, []);
+
+const getCardsAmount = () => {
+  const amount = Number(prompt("Сколько карточек отобразить? От 1 до 5"))
+  
+  if (amount >= 1 && amount <= 5) {
+    return amount
+  }
+  
+  return ('Введите значение от 1 до 5')
+}
+
+amount = getCardsAmount()
+
+let cards = productCard.slice(0, amount)
